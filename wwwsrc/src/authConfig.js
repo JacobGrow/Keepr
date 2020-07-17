9d65b81244397191b90ad0e295ca265cbfc3fc60
Jacob Grow <jacobagrow@gmail.com>
2020-07-17 16:39:34 -0600

    init

---
+++ wwwsrc/src/authConfig.js
@@ -0,0 +1,3 @@
+export const domain = "jakes-domain.us.auth0.com";
+export const audience = "https://Keepr";
+export const clientId = "qI4zreiOCMEhN756rkssDO8u6RVIKQdR";
---
+++ wwwsrc/src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <h1>Welcome to Hell</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "home",
+  computed: {
+    user() {
+      return this.$store.state.user;
+    }
+  },
+  methods: {
+    logout() {
+      this.$store.dispatch("logout");
+    }
+  }
+};
+</script>
