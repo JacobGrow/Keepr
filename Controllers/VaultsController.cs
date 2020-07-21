b5075819c4169e9a3d89ffc0ecc0a3b1de9689a4
Jacob Grow <jacobagrow@gmail.com>
2020-07-20 22:13:56 -0600

    Vaults controller almost done

--- Controllers/VaultsController.cs
+++ Controllers/VaultsController.cs
@@ -1,4 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using keepr.Models;
 using keepr.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 
 namespace keepr.Controllers
 {
@@ -9,5 +16,87 @@ namespace keepr.Controllers
     {
       _vs = vs;
     } 
+
+    [HttpGet("user")]
+    [Authorize]
+    public ActionResult<IEnumerable<Vault>> GetVaultsByUser()
+    {
+      try
+      {
+        string userId = HttpContext.User.FindFirst(ClaimTypes.NameIdentifier).Value;
+        return Ok(_vs.GetByUserId(userId));
+      }
+      catch (Exception e)
+      {
+        return BadRequest(e.Message);
+      };
     }
+    //GETBYID
+    [HttpGet("{id}")]
+    public ActionResult<Vault> Get(int id)
+    {
+      try
+      {
+        return Ok(_vs.Get(id));
+      }
+      catch (Exception e)
+      {
+        return BadRequest(e.Message);
+      }
+    }
+    //GETKeepsByVaultID
+    // [HttpGet("{vaultId}/keeps")]
+    // public ActionResult<IEnumerable<VaultKeep>> GetKeepsByVaultId(int id)
+    // {
+    //   try
+    //   {
+    //     return Ok(_tis.GetIngsByTacoId(id));
+    //   }
+    //   catch (Exception e)
+    //   {
+    //     return BadRequest(e.Message);
+    //   }
+    // }
+    //POST
+    [HttpPost]
+    public ActionResult<Vault> Post([FromBody] Vault newVault)
+    {
+      try
+      {
+        return Ok(_vs.Create(newVault));
+      }
+      catch (Exception e)
+      {
+        return BadRequest(e.Message);
+      }
+    }
+    //PUT
+    [HttpPut("{id}")]
+    public ActionResult<Vault> Edit([FromBody] Vault newVault, int id)
+    {
+      try
+      {
+        newVault.Id = id;
+        return Ok(_vs.Edit(newVault));
+      }
+      catch (Exception e)
+      {
+        return BadRequest(e.Message);
+      }
+    }
+    //DEL
+    [HttpDelete("{id}")]
+    public ActionResult<Vault> Delete(int id)
+    {
+      try
+      {
+        return Ok(_vs.Delete(id));
+      }
+      catch (Exception e)
+      {
+        return BadRequest(e.Message);
+      }
+    }
+
+  }
 }
--- Services/VaultsService.cs
+++ Services/VaultsService.cs
@@ -1,7 +1,33 @@
+using System;
+using keepr.Models;
+
 namespace keepr.Services
 {
-    public class VaultsService
+  public class VaultsService
+  {
+    internal object GetByUserId(string userId)
     {
-        
+      throw new NotImplementedException();
     }
+
+    internal Vault GetById(int id)
+    {
+         throw new NotImplementedException();
+    }
+
+    internal object Create(Vault newVault)
+    {
+      throw new NotImplementedException();
+    }
+
+    internal object Edit(Vault newVault)
+    {
+      throw new NotImplementedException();
+    }
+
+    internal object Delete(int id)
+    {
+      throw new NotImplementedException();
+    }
+  }
 }
